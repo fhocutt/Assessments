229f1729f883d4b40470f579501b2de68e495230
niharika29 <niharikakohli29@gmail.com>
2015-12-16 15:56:04 +0000

    Initial commit for PageAssessments extension
    Parser function data is saved in extensionData until parsing completes
    
    Bug: T121068
    Change-Id: I5ebb1ff2b736f56757648db522de5472e8c2a7c4

---
+++ db/addLoggingTable.sql
@@ -0,0 +1,15 @@
+-- Add article assessments log table
+
+CREATE TABLE IF NOT EXISTS /*_*/page_assessments_log (
+	pa_page_id          INT(10) NOT NULL,
+	pa_project          VARCHAR(128) DEFAULT NULL,
+	pa_class            VARCHAR(20) DEFAULT NULL,
+	pa_importance       VARCHAR(20) DEFAULT NULL,
+	pa_page_revision    INT(10) NOT NULL,
+	pa_user_id          INT(10) DEFAULT NULL,
+	pa_timestamp        BINARY(14) NOT NULL
+)/*$wgDBTableOptions*/;
+
+CREATE INDEX /*i*/pa_project_log ON /*_*/ page_assessments_log (pa_project);
+CREATE INDEX /*i*/pa_timestamp ON /*_*/ page_assessments_log (pa_timestamp);
+CREATE INDEX /*i*/pa_page_project_log ON /*_*/ page_assessments_log (pa_page_id, pa_project);
